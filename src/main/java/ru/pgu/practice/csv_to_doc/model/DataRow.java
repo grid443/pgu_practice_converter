76d19aef48563fa075cc0671cf8f1b31122a200d
Svyatoslav <Svyatoslav1998darkes@gmail.com>
2016-12-16 17:55:02 +0300

    DataRow correction

--- src/main/java/ru/pgu/practice/csv_to_doc/model/DataRow.java
+++ src/main/java/ru/pgu/practice/csv_to_doc/model/DataRow.java
@@ -1,9 +1,6 @@
 package ru.pgu.practice.csv_to_doc.model;
 
-import com.sun.org.apache.xpath.internal.operations.String;
-
 import java.math.BigDecimal;
-import ru.pgu.practice.csv_to_doc.model.Sex;
 
 /**
  * TODO write javaDoc
@@ -15,24 +12,26 @@ public class DataRow {
     private final Sex sex;
     private final BigDecimal salary;
 
-    public String getFIO(){
-        return FIO;
-    }
-    public int getAge(){
-        return age;
-    }
-    public Sex getSex(){
-        return sex;
-    }
-    public BigDecimal getSalary(){
-        return salary;
-    }
-
-//Constructor
-    DaraRow(String F, int A, Sex S, BigDecimal s) {
+    public DataRow(String F, int A, Sex S, BigDecimal s) {
         FIO = F;
         age = A;
         sex = S;
         salary = s;
+    }
+
+    public String getFIO() {
+        return FIO;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Sex getSex() {
+        return sex;
+    }
+
+    public BigDecimal getSalary() {
+        return salary;
     }
 }
