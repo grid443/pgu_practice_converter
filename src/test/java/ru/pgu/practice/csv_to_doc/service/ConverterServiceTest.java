d3d545a4e22043ef096d41c07c19e8b8e19c3a34
Кирилл Васин <kirill.vasin@codeinside.ru>
2016-12-22 17:24:33 +0300

    log timestamp

--- src/test/java/ru/pgu/practice/csv_to_doc/service/ConverterServiceTest.java
+++ src/test/java/ru/pgu/practice/csv_to_doc/service/ConverterServiceTest.java
@@ -6,8 +6,11 @@ import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.lang.reflect.Field;
 import java.net.URL;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 
 public class ConverterServiceTest {
@@ -33,5 +36,22 @@ public class ConverterServiceTest {
 
         ConverterService start = new ConverterService();
         start.start(multipartFile);
+
+
+        Path resultDir = Paths.get(
+            privateStringFieldValue(start, "ROOT_DIR"),
+            privateStringFieldValue(start, "RESULT_DIR")
+        );
+
+    }
+
+    private String privateStringFieldValue(Object object, String fieldName) {
+        try {
+            Field rootDirField = ConverterService.class.getDeclaredField(fieldName);
+            rootDirField.setAccessible(true);
+            return (String) rootDirField.get(object);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
