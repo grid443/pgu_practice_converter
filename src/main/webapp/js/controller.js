190f79d77bcabd54ec8cdd4c00ce0a2cdd175391
vostokova <nbvostokova@gmail.com>
2017-03-01 11:12:23 +0300

    added main and pending html

--- src/main/webapp/js/controller.js
+++ src/main/webapp/js/controller.js
@@ -1,5 +1,17 @@
 'use strict';
 var module = angular.module('converter', []);
 module.controller('controller', function ($scope, $http) {
+    var data = {
+        file: ''
+    };
+
+    $http.post('convert', data).then(
+        function success(response) {
+
+        },
+        function fail(err) {
+
+        }
+    );
 
 });
