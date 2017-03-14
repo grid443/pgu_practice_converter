f07f6b4d411e6b8eb97617ba38a4af9b9155620b
vostokova <nbvostokova@gmail.com>
2017-03-15 01:06:11 +0300

    divided html into views

--- src/main/webapp/js/controlers/controller.js
+++ src/main/webapp/js/controlers/controller.js
@@ -1,20 +1,5 @@
 'use strict';
 var module = angular.module('converter', []);
-
-module.config(function ($stateProvider) {
-    $stateProvider
-        .state('stateName', {
-            url: '/convert',
-            views: {
-                'convert': {
-                    templateUrl: 'js/controllers/startConvert.html',
-                    controller: 'ConvertController'
-                }
-            }
-        });
-});
-
-
 module.controller('ConvertController', function ($scope, $http) {
     console.log('Converter controller');
 
@@ -28,10 +13,21 @@ module.controller('ConvertController', function ($scope, $http) {
 
     $scope.convert = function () {
         $scope.state = 'convert';
-        $scope.stateMessage = 'Обработка файлов началась. Это может занять несколько минут. Или часов...';
+        $scope.stateMessage = 'Обработка файлов началась. Это может занять несколько минут.';
     };
 
     $scope.check = function () {
+        //n - от 0 до 5
+        //Math.random() * (5 - 0) + 0;
+        var n = Math.round(Math.random() * 5);
+
+        if (n == 4) {
+            $scope.stateMessage = 'Во время обработки файлов произошла ошибка';
+            //TODO задизаблить кнопку проверки статуса
+            var btn = angular.element(document.querySelector(".check-btn"));
+            btn.addClass("disabled");
+        }
+
         if (requestCount < 2) {
             $scope.stateMessage = 'Обработка файлов ещё не закончилась. Это может занять несколько минут.';
             requestCount += 1;
---
+++ src/main/webapp/js/controlers/route.js
@@ -0,0 +1,42 @@
+'use strict';
+var module = angular.module('converter', []);
+
+module.config(function ($stateProvider) {
+    $stateProvider
+        .state('start', { //загрузка файлов, переход на convert
+            url: '/start',
+            views: {
+                'convert': {
+                    templateUrl: 'js/views/main/start.html',
+                    controller: 'StartController'
+                }
+            }
+        })
+        .state('processing', { //проверка статуса переход в start(если тыркнули "назад"), success или fail
+            url: '/processing',
+            views: {
+                'convert': {
+                    templateUrl: 'js/views/main/processing.html',
+                    controller: 'ConvertController'
+                }
+            }
+        })
+        .state('success', { //отображение списка файлов, переход в start
+            url: '/success',
+            views: {
+                'convert': {
+                    templateUrl: 'js/views/main/success.html',
+                    controller: 'SuccessController'
+                }
+            }
+        })
+        .state('list', { //TBD
+            url: '/list',
+            views: {
+                'convert': {
+                    templateUrl: 'js/views/list.html',
+                    controller: 'ListController'
+                }
+            }
+        });
+});
